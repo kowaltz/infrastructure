193353c01973a3023ba84b5d22393e6aab5eb4fd
FABS <franciscoabsampaio@gmail.com>
2023-11-07 20:36:22 +0000

    AWS root: Disable OIDC auth

--- aws/root/main.tf
+++ aws/root/main.tf
@@ -1,10 +1,12 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-south-2"
+  /* WITH OIDC
   assume_role_with_web_identity {
     role_arn                = local.root_role_arn
     web_identity_token_file = "/mnt/workspace/spacelift.oidc"
   }
+  */
 }
 
 locals {
