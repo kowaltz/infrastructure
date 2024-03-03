5dc54719e75d831608172e2e116a36ceec0e0715
FABS <franciscoabsampaio@gmail.com>
2024-03-03 20:36:38 +0000

    Change: Account IDs sensitive

--- aws/root-organization/outputs.tf
+++ aws/root-organization/outputs.tf
@@ -1,5 +1,6 @@
 output "aws_account_id_root" {
   value = var.aws_account_id
+  sensitive = true
 }
 
 output "aws_region" {
@@ -10,4 +11,5 @@ output "aws_account_id_infrastructure_env_vms" {
   value = { for env in var.set_of_environments :
     env => module.aws_organizations_account[env].id
   }
+  sensitive = true
 }
