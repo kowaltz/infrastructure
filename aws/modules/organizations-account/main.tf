e9a3a5820214637d1e7523a528b8acf0f8bd70cf
FABS <franciscoabsampaio@gmail.com>
2024-05-06 10:32:49 +0100

    Create spacelift_default roles for insfrastructure using assume_role

--- aws/modules/organizations-account/main.tf
+++ aws/modules/organizations-account/main.tf
@@ -1,6 +1,6 @@
 # This Terraform module is used to create an AWS Organizations account and associated resources.
 # It generates an account name and email based on the provided variables, and creates an AWS Organizations account
-# using the generated name and email. It also creates an IAM role for authenticating Spacelift with the account.
+# using the generated name and email.
 # The module outputs the ID and name of the created AWS Organizations account.
 
 locals {
@@ -17,44 +17,6 @@ resource "aws_organizations_account" "module" {
   close_on_deletion = true
   parent_id         = var.parent_id
   role_name         = var.name
-}
-
-provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::${module.aws_organizations_account["dev"]}:role/vms"
-  }
-
-  alias  = "organizations-account"
-  region = var.aws_region
-}
-
-resource "aws_iam_role" "spacelift_default" {
-  provider    = aws.provider_alias
-  
-  name        = "${var.organization}-role-${var.path}_${var.name}-spacelift_default"
-  description = "Role for authenticating Spacelift with default methods, not OIDC, to ${var.path}'s ${var.name} account."
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : [
-            "324880187172"  # Spacelift's own AWS ID
-          ]
-        },
-        "Action" : "sts:AssumeRole",
-        "Condition" : {
-          "StringLike" : {
-            "sts:ExternalId" : var.list_of_stack_permissions
-          }
-        }
-      }
-    ]
-  })
 }
 
 output "id" {
--- aws/root-organization/infrastructure_env_vms.tf
+++ aws/root-organization/infrastructure_env_vms.tf
@@ -1,7 +1,31 @@
 locals {
-  map_of_environments_and_providers = {
-    "dev" = aws.dev
-    "prod" = aws.prod
+  infrastructure_env_vms_path = {
+    for env in var.set_of_environments: env => "root_infrastructure_${env}"
+  }
+
+  infrastructure_env_vms_assume_role_policy = {
+    for env in var.set_of_environments: env => jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "AWS" : [
+              "324880187172"  # Spacelift's own AWS ID
+            ]
+          },
+          "Action" : "sts:AssumeRole",
+          "Condition" : {
+            "StringLike" : {
+              "sts:ExternalId" : [
+                "${var.organization}-github@*@${var.organization}-stack-${env}-aws_infrastructure_vms@*",
+                "${var.organization}-github@*@${var.organization}-stack-${env}-spacelift@*",
+              ]
+            }
+          }
+        }
+      ]
+    })
   }
 }
 
@@ -9,15 +33,43 @@ module "aws_organizations_account" {
   for_each = var.set_of_environments
   source   = "../modules/organizations-account"
 
-  #provider_to_assume = map_of_environments_and_providers[each.value]
-  
-  list_of_stack_permissions = [
-    "${var.organization}-github@*@${var.organization}-stack-${each.value}-aws_infrastructure_vms@*",
-    "${var.organization}-github@*@${var.organization}-stack-${each.value}-spacelift@*",
-  ]
   name              = "vms"
   parent_id         = aws_organizations_organizational_unit.infrastructure-env[each.value].id
-  path              = "root_infrastructure_${each.value}"
+  path              = local.infrastructure_env_vms_path[each.value]
   organization      = var.organization
   unique_identifier = local.unique_identifier
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::${module.aws_organizations_account["dev"]}:role/vms"
+  }
+
+  alias  = "infrastructure_dev_vms"
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "infrastructure_dev_vms-spacelift_default" {
+  provider    = aws.infrastructure_dev_vms
+  
+  name        = "${var.organization}-role-${local.infrastructure_env_vms_path["dev"]}_vms-spacelift_default"
+  description = "Role for authenticating Spacelift with default methods, not OIDC, to ${local.infrastructure_env_vms_path["dev"]}'s vms account."
+  assume_role_policy = local.infrastructure_env_vms_assume_role_policy["dev"]
+}
+
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::${module.aws_organizations_account["prod"]}:role/vms"
+  }
+
+  alias  = "infrastructure_prod_vms"
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "infrastructure_prod_vms-spacelift_default" {
+  provider    = aws.infrastructure_prod_vms
+  
+  name        = "${var.organization}-role-${local.infrastructure_env_vms_path["prod"]}_vms-spacelift_default"
+  description = "Role for authenticating Spacelift with default methods, not OIDC, to ${local.infrastructure_env_vms_path["prod"]}'s vms account."
+  assume_role_policy = local.infrastructure_env_vms_assume_role_policy["prod"]
 }
