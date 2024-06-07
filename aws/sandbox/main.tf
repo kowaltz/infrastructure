d9a2cd28638ba4a121daa3f992240f27107919fb
FABS <franciscoabsampaio@gmail.com>
2024-06-07 10:33:01 +0100

    Attach cloud integration to sandbox stack

--- aws/sandbox/main.tf
+++ aws/sandbox/main.tf
@@ -1,12 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-  /* WITH OIDC
-  assume_role_with_web_identity {
-    role_arn                = local.root_role_arn
-    web_identity_token_file = "/mnt/workspace/spacelift.oidc"
-  }
-  */
 }
 
 
