41633b15e07c5fef246b6e56d08f274f074ad44c
FABS <franciscoabsampaio@gmail.com>
2024-06-11 18:07:09 +0100

    Fix: gramt to root role

--- aws/root/organization/policy_org_read.tf
+++ aws/root/organization/policy_org_read.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy_attachment" "org_read" {
   name       = "org_read"
-  roles      = ["AWSOrganizationsReadOnlyAccess"]
-  policy_arn = aws_iam_policy.org_create.arn
+  roles      = [var.root_role_name]
+  policy_arn = "arn:aws:iam::aws:policy/AWSOrganizationsReadOnlyAccess"
 }
