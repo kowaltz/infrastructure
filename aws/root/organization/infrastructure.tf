83ccd23cbb2785d8c08eceae28db26c4f665c780
FABS <franciscoabsampaio@gmail.com>
2024-06-11 21:59:24 +0100

    Refactor: Reduce OU name complexity

--- aws/root/organization/infrastructure.tf
+++ aws/root/organization/infrastructure.tf
@@ -1,16 +1,16 @@
 resource "aws_organizations_organizational_unit" "infrastructure" {
-  name      = "${var.organization}-ou-root-infrastructure"
+  name      = "${var.organization}-ou-infrastructure"
   parent_id = aws_organizations_organizational_unit.root.id
 }
 
-resource "aws_organizations_organizational_unit" "infrastructure-env" {
+resource "aws_organizations_organizational_unit" "infrastructure_env" {
   for_each  = var.set_of_environments
-  name      = "${var.organization}-ou-root_infrastructure-${each.value}"
+  name      = "${var.organization}-ou-infrastructure_${each.value}"
   parent_id = aws_organizations_organizational_unit.infrastructure.id
 }
 
 locals {
   infrastructure_env_path = {
-    for env in var.set_of_environments : env => "root_infrastructure_${env}"
+    for env in var.set_of_environments : env => "infrastructure_${env}"
   }
 }
